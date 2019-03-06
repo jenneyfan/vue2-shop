c9b74c6311ebd54cd6ee135f85fd1c26c309aecc
Administrator <fanjunni2006@163.com>
2019-03-06 11:49:22 +0800

    GoodList组件拆分

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import GoodsList from '@/views/GoodsList'
 
 Vue.use(Router)
@@ -9,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GoodsList',
+      component: GoodsList
     },
     {
       path: '/GoodsList',
