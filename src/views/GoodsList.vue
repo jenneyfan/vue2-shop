5e387a6a447056da003cfb65aaffc3af1b7f2b85
Administrator <fanjunni2006@163.com>
2019-03-06 14:27:13 +0800

    mock数据对接

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -21,18 +21,17 @@
               </dd>
             </dl>
           </div>
-
           <!-- search result accessories list -->
           <div class="accessory-list-wrap">
             <div class="accessory-list col-4">
               <ul>
-                <li>
+                <li v-for="item in goodsList">
                   <div class="pic">
-                    <a href="#"><img src="/static/img/1.jpg" alt=""></a>
+                    <a href="#"><img :src="'/static/img/' + item.productImage" alt=""></a>
                   </div>
                   <div class="main">
-                    <div class="name">小米电视4 55英寸</div>
-                    <div class="price">3999</div>
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.salePrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
@@ -54,17 +53,29 @@
   import NavHeader from './../components/NavHeader.vue'
   import NavBread from './../components/NavBread.vue'
   import NavFooter from './../components/NavFooter.vue'
+  import axios from 'axios'
   export default {
     name: 'GoodsList',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        goodsList:[]
       }
     },
     components:{
       NavHeader,
       NavBread,
       NavFooter
+    },
+    mounted(){
+      this.getGoodsList();
+    },
+    methods:{
+      getGoodsList(){
+        axios.get('/static/mock/goods.json')
+          .then((result)=>{
+            this.goodsList = result.data.result;
+          })
+      }
     }
   }
 </script>
